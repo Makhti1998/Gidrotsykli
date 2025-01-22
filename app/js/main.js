0a76740c70c3286c941e53e7599f5b93d52b5dde
Makhti <msayfidarow@gmail.com>
2025-01-22 13:50:40 +0300

    -asearch tabs

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,22 @@
-$(function () {
+const swiper = new Swiper(".banner-slider", {
+  rewind: true,
+  spaceBetween: 0,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
-})
+$(".search__tabs-item").on("click", function (e) {
+  e.preventDefault();
+
+  $(".search__tabs-item").removeClass("search__tabs-item--active");
+  $(".search__content-item").removeClass("search__content-item--active");
+
+  $(this).addClass("search__tabs-item--active");
+  $($(this).attr("href")).addClass("search__content-item--active");
+});
